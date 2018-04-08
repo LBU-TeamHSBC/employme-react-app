71799c11025593d001b19dde613a4daa9c205254
Ed Brook <edbrook@gmail.com>
2018-04-08 16:01:19 +0100

    placeholder text for home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,16 @@ export function Home(props) {
   return (
     <div className="content">
       <Link to="/login">---- LOGIN ----</Link>
+      <hr />
+      <p style={{width: "600px", textAlign: "justify"}}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quam eros,
+        tempus porttitor odio eu, molestie consequat nibh. Duis placerat ultrices
+        metus, sed pellentesque tellus congue et. Phasellus molestie condimentum
+        auctor. Vestibulum vel arcu leo. Integer odio odio, tincidunt sed
+        vestibulum id, varius non arcu. Morbi eget purus gravida, vehicula ligula
+        in, iaculis nisi. Sed quis dui est. Suspendisse sed dolor volutpat,
+        porttitor tellus efficitur, maximus mi. Ut tempus turpis purus.
+      </p>
     </div>
   );
 }
