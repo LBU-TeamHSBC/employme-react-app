09e55df9c6ce33b41a7ef3f4d204f6c46a640c63
Ed Brook <edbrook@gmail.com>
2018-04-08 12:42:20 +0100

    removed unused section

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,7 +6,7 @@ import { ScoreCard } from './ScoreCard';
 import { TopSkillsCard } from './TopSkillsCard';
 
 export function Dashboard(props) {
-  const dummy_card = {
+  const dummy_overview_data = {
     score:3247,
     matched_companies:7,
     courses_completed:14,
@@ -19,7 +19,7 @@ export function Dashboard(props) {
     <article className="content dashboard-page">
       <section className="section">
         <div className="row sameheight-container">
-          {OverviewCard(dummy_card)}
+          {OverviewCard(dummy_overview_data)}
           <ScoreCard />
         </div>
       </section>
@@ -27,14 +27,6 @@ export function Dashboard(props) {
         <div className="row sameheight-container">
           <EnrolledCard />
           <TopSkillsCard />
-        </div>
-      </section>
-      <section className="section map-tasks">
-        <div className="row sameheight-container">
-          <div className="col-md-8">
-          </div>
-          <div className="col-md-4">
-          </div>
         </div>
       </section>
     </article>
