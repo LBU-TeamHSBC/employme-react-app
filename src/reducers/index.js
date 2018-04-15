0c0875514f97fd6d4a7908d8cc5ee2be37139f05
Ed Brook <edbrook@gmail.com>
2018-04-15 20:21:25 +0100

    short-term-fix Mark as logged in if we have a token

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,13 @@
 import { combineReducers } from 'redux';
 
-const isLoggedIn = (state=false, action) => {
+const getDefaultState = _ => {
+  if (sessionStorage.getItem('token')) {
+    return true;
+  }
+  return false;
+}
+
+const isLoggedIn = (state=getDefaultState(), action) => {
   switch(action.type) {
     case 'IS_LOGGED_IN':
       return true;
